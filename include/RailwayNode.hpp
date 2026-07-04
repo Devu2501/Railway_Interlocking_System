91a42d68e81b3133686a5f4324945805c8ec6f95
Devesh Kumar <devesh006kumar@gmail.com>
2026-07-04 23:51:09 +0530

    Add RailwayNode Header file

---
+++ include/RailwayNode.hpp
@@ -0,0 +1,51 @@
+#pragma once 
+
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+//different types of railway nodes
+enum class RailwayNodeType
+{
+   SWITCH=0,
+   SIGNAL,
+   PLATFORM,
+   ENTRY,
+   EXIT
+};
+
+class Track; //Forward declaration of Track class
+
+class RailwayNode
+{
+protected:
+    int node_id;
+
+    string name;
+    
+    RailwayNodeType type;
+
+    vector<Track*> connectedTracks; //Vector to store connected tracks
+
+public:
+
+   RailwayNode(int id, const string& name, RailwayNodeType type);
+
+    virtual ~RailwayNode()=default;
+
+    int getNodeId() const;
+
+    string getName() const;
+
+    RailwayNodeType getType() const;
+
+    const vector<Track*>& getConnectedTracks() const;
+
+    void addTrack(Track* track);
+
+    virtual void printNodeInfo() const = 0; //Pure virtual function to print node information  
+
+
+};
